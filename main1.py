f13fe8e9cbbe56c57bd30a80cd1f069f54afeced
Vlad Oreshko <orex3896@gmail.com>
2023-03-21 12:36:25 +0300

    Created DZ4

---
+++ main1.py
@@ -0,0 +1,26 @@
+# list1=[]
+# for i in range(1,10,2):
+#     list1.append(i)
+# print(list1)
+# list_1 = [i for i in range(2,10,2) if i %2 ==0]
+# print(list_1)   
+# list_1 = list()
+# for i in range(5):
+#     n = int(input())
+#     list_1.append(n)
+# print(list_1)
+# a = 3
+# b = 11
+# s = 2022
+# print(a,b,s)
+# print(a,"-",b,'-',s)
+# print("{} - {} - {}".format(a,b,s))
+# print(f'first - {a} second - {b} third - {s}')
+
+# a = int(input("Введите 1 число"))
+# b = int(input("Введите 2 число"))
+# print(a,"+",b, "=", a+b )
+
+d = 1.345
+print(str(d)*2)
+  
