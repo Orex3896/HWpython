4037ed477a0da0cb1bf3b31fd1817729585bc3c6
Vlad Oreshko <orex3896@gmail.com>
2023-03-28 01:27:18 +0300

    Created DZ6r

--- HW6.py
+++ HW6.py
@@ -10,31 +10,25 @@
 # for i in range(N):
 #     print(x+i*d, end = " ")# программа идёт так 7 + 0*2=> 7; 7+1*2=>9 и до N
 
-# list_1 = []
-# for i in input():
-#     list_1.append(i)
-# print(list_1)
-# num_min=int("Введите минимальное значение списка = "+input())
-# num_max=int("Введите максимальное значение списка = "+input())
-# for ind in ind:
-#     if num_min<=val<=num_max:
-#         val in enumerate(list_1)
-# print(list_1)
 
+# Задача 32: Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не
+# меньше заданного минимума и не больше заданного
+# максимума)
 from random import randint
  
-lo, hi = 5, 15
+min, max = 5, 15
 #data = [randint(1, 10) for _ in range(20)]
 data=[-5, 9, 0, 3, -1, -2, 1,
 4, -2, 10, 2, 0, -9, 8, 10, -9,
 0, -5, -5, 7]
 print("Original array:", data, sep='\n')
-indexes = [i for i, v in enumerate(data) if lo <= v <= hi]
+indexes = [i for i, v in enumerate(data) if min <= v <= max]
 print("Indexes:", indexes, sep='\n')
 result = []
 i = len(indexes)
 while i :
     i -= 1
-    result.append(data.pop(indexes[i]))
-print("Remaining elements:", data, sep='\n')
-print("Required elements:", result, sep='\n')
+    result.append(data.pop(indexes[i]))#от всего списка отнимаются нужные элементы
+print("Остальные элементы:", data, sep='\n')
+print("Необходимые элементы:", result, sep='\n')
